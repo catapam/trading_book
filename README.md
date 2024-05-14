47415cbbf13a25b914148bf30d7ee8ae5b3a885c
catapam <matheuscatapam@gmail.com>
2024-05-14 23:45:09 +0000

    Regroup and organize functions

--- README.md
+++ README.md
@@ -1,32 +1,11 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
-
-Welcome,
-
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **March 14, 2023**
-
-## Reminders
-
-- Your code must be placed in the `run.py` file
-- Your dependencies must be placed in the `requirements.txt` file
-- Do not edit any of the other files or your code may not deploy properly
-
-## Creating the Heroku app
-
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
-
-1. `heroku/python`
-2. `heroku/nodejs`
-
-You must then create a _Config Var_ called `PORT`. Set this to `8000`
-
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
-
-Connect your GitHub repository and deploy as normal.
-
-## Constraints
-
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
-
----
-
-Happy coding!
+> readding a key that is already set udpates it
+> input checking is being deleted if not validating but not erroing and asking the input again
+> if main menu call starts with ./, delete it and continue
+> initiate validations by setting the values that are accompanied by key, like key:value and then proceed to the ones without key
+> if a value is already present and you are going to update it, a y/n question should come up for confirmation on the change
+> OOP conversion
+> other menu functions: set,check,recalc (more on help)
+> bulk mode
+> database input
+> calculations
+> database update
--- run.py
+++ run.py
@@ -64,97 +64,8 @@ QUESTION = StyleOutput.cyan
 SUCCESS= StyleOutput.green_background
 ERROR= StyleOutput.error    
 
-def exit_program():
-    """
-    Closes the program
-    """
-    # print("\n")
-    print(ERROR("\nAll non-saved data will be lost"))
-    leave = get_input(italic(red("Would you like to quit the program anyways? (y/n)\n")))
-    confirmation = yes_or_no(leave)
-    
-    if confirmation is True:
-        print("\nClosing...")
-        print(GREETING("See you next time!"))
-        exit()
 
-
-def show_help(context=None):
-    """
-    Show help menu
-    """
-    if context == "help":
-        context=None
-    
-    while True:
-        if context is None:
-            print(TITLE("Help Information:"))
-            print("  - Type 'entry' to enter a trade")
-            print("  - Type 'check' to view stats")
-            print("  - Type 'set' to open settings")
-            print("  - Type 'help' to get help")
-            print("  - Type 'back' to return to previous location")
-            print("  - Type 'cancel' to cancel current job")
-            print("  - Type 'exit' to quit the program")
-            print("\nCommands described above can be ran from anywhere in the program.")
-            return False
-        
-        else:
-            print(TITLE(f"Help for '{context}':"))
-            #context specifics placeholder
-            print(" - Type 'back' to return to where you were")
-            print(" - Type 'cancel' to cancel current job and go back to main menu")
-            print(" - Type 'help' again to see general help")
-            cmd=get_input("\nEnter command: \n")
-            
-            if cmd == 'back':
-                return False
-            elif multi_menu_call(cmd,True):
-                multi_menu_call(cmd,context="help")
-            else:
-                print(italic(red("\nNot a valid command!")))
-                continue     
-        
-        
-def process_command(cmd,child_command=None,context=None):
-    """
-    Process commands from command line
-    """
-    validator=True
-    
-    if cmd == "exit":
-        validator = exit_program(*child_command) if child_command else exit_program()
-    elif cmd == "check":
-        validator = check_stats(*child_command) if child_command else check_stats()
-    elif cmd == "help":
-        if child_command:
-            validator = show_help(*child_command)
-        else:
-            validator = show_help(context) if context else show_help()
-    else:
-        if not context:
-            if cmd == "entry":
-                validator = log_trade(*child_command) if child_command else log_trade()
-            elif cmd == "set":
-                validator = manage_settings(*child_command) if child_command else manage_settings()
-            elif cmd == "cancel":
-                validator = navigate_away()  
-            elif cmd == "back":
-                print(italic(red("\nThere is no where to go back to, you are in home page.")))
-                print("Type 'help' to see menu options.")  
-        else:
-            if navigate_away() is True:
-                print(SUCCESS("\nLet's continue with a new command!"))
-                if cmd == "entry":
-                    validator = log_trade(*child_command) if child_command else log_trade()
-                elif cmd == "set":
-                    validator = manage_settings(*child_command) if child_command else manage_settings()
-            else:
-                validator=False
-                        
-    return validator 
-
-
+# General start
 def get_input(prompt):
     """
     Get user's input to the command line
@@ -163,6 +74,33 @@ def get_input(prompt):
     return user_input.strip().lower()
            
 
+def delete_none_values(trade_details,num):
+    """
+    Deletes any None values from an array
+    """
+    trade_values = [value[num] for value in trade_details.values()]
+    filtered_trade_values = [str(item) for item in trade_values if item is not None]
+    
+    return filtered_trade_values    
+
+
+def yes_or_no(input):
+    """
+    Create the yes and no menu option for confirmation requests
+    """
+    while True:
+        if input in ('y','yes','yeap','yeah','ya','ye','yy','yees','yess'):
+            return True
+        elif input in ('n','no','nope','nah','not','dont','nn','nno','noo'):
+            return False
+        else:
+            print(ERROR("\nPlease use 'y' or 'n'."))
+            input = get_input("\nEnter response:\n")
+            continue
+# General end
+
+
+# Validations start
 def input_check(prompt,format=None):
     """
     Check if input is menu or not and push for format validations
@@ -325,29 +263,6 @@ def format_validation(prompt,format, silent=False):
         return None
     
 
-def navigate_away():
-    """
-    Prompt confirmation request when moving away from running job
-    """
-    cancel = get_input(italic(red("\nNavigating away will cancel the current job. Do you want to proceed? (y/n):\n")))
-    return yes_or_no(cancel)
-    
-        
-def yes_or_no(input):
-    """
-    Create the yes and no menu option for confirmation requests
-    """
-    while True:
-        if input in ('y','yes','yeap','yeah','ya','ye','yy','yees','yess'):
-            return True
-        elif input in ('n','no','nope','nah','not','dont','nn','nno','noo'):
-            return False
-        else:
-            print(ERROR("\nPlease use 'y' or 'n'."))
-            input = get_input("\nEnter response:\n")
-            continue
-       
-
 def auto_validator(data):
     """
     Auto-validate data against formatting, fixing possible syntax errors on user entry
@@ -407,8 +322,10 @@ def reconstruct_trade_details(validated, invalidated, original_data):
                     break
 
     return trade_details
+# Validations end
         
-                
+
+# Menu start                
 def log_trade(action=None, type=None, price=None, stop=None, atr=None):
     """
     Log a trade with optional user interaction for details.
@@ -462,15 +379,14 @@ def log_trade(action=None, type=None, price=None, stop=None, atr=None):
                     break
                 
                 if not multi_menu_call(prompt, True, context=cmd):
-                    if len(words) <= len(key_none) + 1:
-                        for word,key in zip(words,key_none):
-                            fmt = trade_details[key][0]
-                            trade_details[key] = (fmt,word)
-                        key_none=[]
-                        words=[]
-                    else:
-                        print("Too many arguments input, any extra arguments will be ignored.")
-                        continue
+                    if len(words) > len(key_none):
+                        print(ERROR("\nToo many arguments input, any extra arguments will be ignored."))
+                        words = words[:len(key_none)]
+                    for word,key in zip(words,key_none):
+                        fmt = trade_details[key][0]
+                        trade_details[key] = (fmt,word)
+                    key_none=[]
+                    words=[]
                 else:
                     if not multi_menu_call(prompt, context=cmd):
                         stop_process=False
@@ -495,16 +411,6 @@ def log_trade(action=None, type=None, price=None, stop=None, atr=None):
         action, type, price, stop, atr = (trade_details[k][1] for k in trade_details)
         print(SUCCESS(f"\nTrade logged with user input:\nentry {action} {type} {price} {stop} {atr}"))
 
-
-def delete_none_values(trade_details,num):
-    """
-    Deletes any None values from an array
-    """
-    trade_values = [value[num] for value in trade_details.values()]
-    filtered_trade_values = [str(item) for item in trade_values if item is not None]
-    
-    return filtered_trade_values    
-
        
 def view_stats():
     """
@@ -526,6 +432,106 @@ def check_stats():
     Check all trades active and curent stats of the trading strategy
     """
     print("Current Trading Stats:")
+
+
+def exit_program():
+    """
+    Closes the program
+    """
+    # print("\n")
+    print(ERROR("\nAll non-saved data will be lost"))
+    leave = get_input(italic(red("Would you like to quit the program anyways? (y/n)\n")))
+    confirmation = yes_or_no(leave)
+    
+    if confirmation is True:
+        print("\nClosing...")
+        print(GREETING("See you next time!"))
+        exit()
+
+
+def navigate_away():
+    """
+    Prompt confirmation request when moving away from running job
+    """
+    cancel = get_input(italic(red("\nNavigating away will cancel the current job. Do you want to proceed? (y/n):\n")))
+    return yes_or_no(cancel)
+
+
+def show_help(context=None):
+    """
+    Show help menu
+    """
+    if context == "help":
+        context=None
+    
+    while True:
+        if context is None:
+            print(TITLE("Help Information:"))
+            print("  - Type 'entry' to enter a trade")
+            print("  - Type 'check' to view stats")
+            print("  - Type 'set' to open settings")
+            print("  - Type 'help' to get help")
+            print("  - Type 'back' to return to previous location")
+            print("  - Type 'cancel' to cancel current job")
+            print("  - Type 'exit' to quit the program")
+            print("\nCommands described above can be ran from anywhere in the program.")
+            return False
+        
+        else:
+            print(TITLE(f"Help for '{context}':"))
+            #context specifics placeholder
+            print(" - Type 'back' to return to where you were")
+            print(" - Type 'cancel' to cancel current job and go back to main menu")
+            print(" - Type 'help' again to see general help")
+            cmd=get_input("\nEnter command: \n")
+            
+            if cmd == 'back':
+                return False
+            elif multi_menu_call(cmd,True):
+                multi_menu_call(cmd,context="help")
+            else:
+                print(italic(red("\nNot a valid command!")))
+                continue     
+        
+        
+def process_command(cmd,child_command=None,context=None):
+    """
+    Process commands from command line
+    """
+    validator=True
+    
+    if cmd == "exit":
+        validator = exit_program(*child_command) if child_command else exit_program()
+    elif cmd == "check":
+        validator = check_stats(*child_command) if child_command else check_stats()
+    elif cmd == "help":
+        if child_command:
+            validator = show_help(*child_command)
+        else:
+            validator = show_help(context) if context else show_help()
+    else:
+        if not context:
+            if cmd == "entry":
+                validator = log_trade(*child_command) if child_command else log_trade()
+            elif cmd == "set":
+                validator = manage_settings(*child_command) if child_command else manage_settings()
+            elif cmd == "cancel":
+                validator = navigate_away()  
+            elif cmd == "back":
+                print(italic(red("\nThere is no where to go back to, you are in home page.")))
+                print("Type 'help' to see menu options.")  
+        else:
+            if navigate_away() is True:
+                print(SUCCESS("\nLet's continue with a new command!"))
+                if cmd == "entry":
+                    validator = log_trade(*child_command) if child_command else log_trade()
+                elif cmd == "set":
+                    validator = manage_settings(*child_command) if child_command else manage_settings()
+            else:
+                validator=False
+                        
+    return validator 
+# Menu end   
     
     
 def main_loop():
